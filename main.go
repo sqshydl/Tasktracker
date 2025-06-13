0a3b3a69e586cb78292164815e052ce0a1dcb034
Tora <mochammadtora@gmail.com>
2025-06-14 05:13:15 +0700

    Add task creation prompt and JSON tags for Task struct

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 type Task struct {
-	AutoId      int
-	Description string
-	Status      int
-	CreatedAt   time.Time
-	UpdateAt    time.Time
+	AutoId      int       `json:"id"`
+	Description string    `json:"description"`
+	Status      int       `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdateAt    time.Time `json:"updated_at"`
 }
 
 var currentId int = 0
@@ -19,7 +23,11 @@ func getNextId() int {
 	return currentId
 }
 
-func add(newTask string) {
+func add() Task {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Insert New Task: ")
+	desc, _ := reader.ReadString('\n')
+	desc = strings.TrimSpace(desc)
 
 }
 
